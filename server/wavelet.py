25fe6754a57e27282ea23b4c2a51be234aeb34b5
Rohti Dudhal <61742890+rohit-dudhal@users.noreply.github.com>
2020-06-23 17:06:58 +0530

    Add files via upload

---
+++ server/wavelet.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 17:39:32 2020
+
+@author: RAW__HIT
+"""
+
+import numpy as np
+import pywt
+import cv2
+
+def w2d(img, mode='haar',level=1):
+    imArray = img
+    imArray = cv2.cvtColor(imArray,cv2.COLOR_RGB2GRAY)
+    imArray = np.float32(imArray)
+    imArray /=255;
+    coeffs=pywt.wavedec2(imArray, mode, level=level)
+    
+    coeffs_H=list(coeffs)
+    coeffs_H[0] *=0;
+    
+    imArray_H = pywt.waverec2(coeffs_H, mode);
+    imArray_H *= 255;
+    imArray_H = np.uint8(imArray_H)
+    
+    return imArray_H
